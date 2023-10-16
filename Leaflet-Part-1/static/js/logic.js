f61f53466ac71c3a7cf1b98c3177b1c322a732b9
crystalleelucas <134230685+crystalleelucas@users.noreply.github.com>
2023-10-16 17:42:13 -0400

    Add files via upload

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,103 @@
+// Define the url for the GeoJSON earthquake data
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Create the map.
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+});
+
+// Add a tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Retrieve and add the earthquake data to the map
+d3.json(url).then(function(data) {
+    function mapStyle (feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: mapColor(feature.geometry.coordinates[2]),
+            color: "black",
+            radius: mapRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+
+    
+    }
+    //Colors for depth
+    function mapColor(depth) {
+        switch (true) {
+            case depth > 90:
+                return "red";
+            case depth > 70:
+                return "orangered";
+            case depth > 50:
+                return "orange";
+            case depth > 30:
+                return "gold";
+            case depth > 10:
+                return "yellow";
+            default:
+                return "lightgreen";
+        }
+    }
+    //Establish the magnitude size.
+    function mapRadius(mag) {
+        if (mag === 0) {
+            return 1;
+        }
+        return mag * 4;
+    }
+    // Add earthquake data to the map.
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+
+        style: mapStyle,
+
+        //Activate pop-up data when circles are clicked
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
+
+        }
+
+
+    }).addTo(myMap);
+
+// Create legend with colors to corrolate with depth
+var legend = L.control({position: "bottomright"});
+
+legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+
+    
+
+    let grades = [-10, 10, 30, 50, 70, 90];
+    let colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"];
+
+
+      
+
+    // Loop through our intervals and generate a label with a colored square for each interval.
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: "
+        + colors[i]
+        + "'></i> "
+        + grades[i]
+        + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+
+  };
+  legend.addTo(myMap)
+  });
